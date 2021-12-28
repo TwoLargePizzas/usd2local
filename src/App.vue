c5869140e537f65cc8909f43abe5564410e89e0e
Dylan Wilson <dylan@looponce.com>
2021-12-28 17:28:42 +1000

    cors testing hack

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <v-app>
     <v-main>
-      <app-layout>
+      <!-- <app-layout>
         <router-view></router-view>
-      </app-layout>
+      </app-layout> -->
+      <pre>
+        {{ hello }}
+      </pre>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import AppLayout from "@/layouts/AppLayout.vue";
+// import AppLayout from "@/layouts/AppLayout.vue";
+import axios from "axios";
 
 export default {
-  components: { AppLayout },
+  // components: { AppLayout },
   name: "App",
+
+  data() {
+    return {
+      hello: "world",
+    };
+  },
+
+  async created() {
+    const response = await axios.get("https://localhost:5001/api/blockchains");
+    console.info(response);
+  },
 };
 </script>
